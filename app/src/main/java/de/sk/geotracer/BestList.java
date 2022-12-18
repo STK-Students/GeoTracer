09db7c6fd7f2c5905644b18829867a11de6ce315
sevi228 <severin.metternich@hotmail.com>
2022-12-18 16:00:03 +0100

    Added Bestenliste

--- app/src/main/java/de/sk/geotracer/BestList.java
+++ app/src/main/java/de/sk/geotracer/BestList.java
@@ -3,12 +3,31 @@ package de.sk.geotracer;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
 
 public class BestList extends AppCompatActivity {
+
+    //Parameter, die in der Bestenliste angezeigt werden soll.
+    String listPlatz [] = {"Platz 1", "Platz 2", "Platz 3", "Platz 4", "Platz 5", "Platz 6", "Platz 7", };
+
+    //ListView
+    ListView bestListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_best_list);
+
+        //Lokalisieren der ListView
+        bestListView = (ListView) findViewById(R.id.bestListView);
+
+        //Erstellen des Adapters für die Parameterübergabe
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, R.layout.activity_best_list_view, R.id.TextView, listPlatz);
+        bestListView.setAdapter(arrayAdapter);
+
     }
+
+
 }
---
+++ app/src/main/java/de/sk/geotracer/BestListView.java
@@ -0,0 +1,14 @@
+package de.sk.geotracer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BestListView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_best_list_view);
+    }
+}
