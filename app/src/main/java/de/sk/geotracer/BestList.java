38eee6386dd5f5298c2f0bfbd462754f4a83fbf0
moritz <moritz.schoppe@proton.me>
2023-01-13 14:18:41 +0100

    add gettripitems to bestlist

--- app/src/main/java/de/sk/geotracer/BestList.java
+++ app/src/main/java/de/sk/geotracer/BestList.java
@@ -1,10 +1,25 @@
 package de.sk.geotracer;
 
+import static android.content.ContentValues.TAG;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 
 public class BestList extends AppCompatActivity {
@@ -13,6 +28,9 @@ public class BestList extends AppCompatActivity {
     String listPlatz [] = {"platz 1", "platz 2", "platz 3"};
     //Bilder, die in der Bestenliste angezeigt werden sollen.
     int listImages []= {R.drawable.platz1, R.drawable.platz2, R.drawable.platz3};
+    FirebaseAuth mAuth = FirebaseAuth.getInstance();
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
+    FirebaseUser user = mAuth.getCurrentUser();
 
     //ListView
     ListView bestListView;
@@ -20,6 +38,7 @@ public class BestList extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getTripItems();
         setContentView(R.layout.activity_best_list);
         //Lokalisieren der ListView
         bestListView = (ListView) findViewById(R.id.bestListView);
@@ -34,4 +53,25 @@ public class BestList extends AppCompatActivity {
         */
 
     }
+
+    protected void getTripItems(){
+        db.collection(user.getUid())
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                Log.d(TAG, document.getId() + " => " + document.getData());
+                                System.out.println(document.getId() + " => " + document.getData());
+                            }
+                        } else {
+                            Log.d(TAG, "Error getting documents: ", task.getException());
+                        }
+                    }
+                });
+    }
+
+
+
 }
