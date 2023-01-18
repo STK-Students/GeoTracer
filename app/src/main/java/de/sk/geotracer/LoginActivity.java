b0e9cd73f5043dd71995564330a0ce5effaee46b
saltyaimbotter <saltyaimbotter@gmail.com>
2023-01-18 13:40:20 +0100

    login cleanup

--- app/src/main/java/de/sk/geotracer/LoginActivity.java
+++ app/src/main/java/de/sk/geotracer/LoginActivity.java
@@ -1,43 +1,24 @@
 package de.sk.geotracer;
-import static android.content.ContentValues.TAG;
 
 import android.content.Intent;
 import android.os.Bundle;
-
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.android.material.snackbar.Snackbar;
-import com.google.firebase.auth.AuthResult;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.firestore.FirebaseFirestore;
-
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.util.Log;
-import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
-import org.w3c.dom.Text;
-
-import java.util.HashMap;
-import java.util.Map;
+import com.google.firebase.auth.FirebaseAuth;
 
 import de.sk.geotracer.databinding.ActivityLoginBinding;
-import de.sk.geotracer.maps.MapsActivity;
 
 public class LoginActivity extends AppCompatActivity {
 
     private AppBarConfiguration appBarConfiguration;
-    private ActivityLoginBinding binding;
     private FirebaseAuth mAuth;
 
 
@@ -45,86 +26,43 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-
-        binding = ActivityLoginBinding.inflate(getLayoutInflater());
+        ActivityLoginBinding binding = ActivityLoginBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-
         setSupportActionBar(binding.toolbar);
 
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_login);
         appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
 
-        binding.fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-
-        // Initialize Firebase Auth
         mAuth = FirebaseAuth.getInstance();
     }
-
 
     @Override
     protected void onStart() {
         super.onStart();
 
-        FirebaseUser currentUser = mAuth.getCurrentUser();
-        if(currentUser != null){
-            System.out.println("Hauptseite");
-        }
-
-
-
         Button loginButton = (Button) findViewById(R.id.btnLogin);
-        loginButton.setOnClickListener(new View.OnClickListener() {
+        loginButton.setOnClickListener(view -> {
+            TextView tvEmail = (TextView) findViewById(R.id.tvEmail);
+            TextView tvPassword = (TextView) findViewById(R.id.tvPassword);
 
-            @Override
-            public void onClick(View view) {
-                FirebaseFirestore db = FirebaseFirestore.getInstance();
-                TextView tvEmail = (TextView) findViewById(R.id.tvEmail);
-                TextView tvPassword = (TextView) findViewById(R.id.tvPassword);
+            String emailText = tvEmail.getText().toString();
+            String passwordText = tvPassword.getText().toString();
 
-                String emailText = tvEmail.getText().toString();
-                String passwordText = tvPassword.getText().toString();
-
-                //Test
-                Map<String, Object> city = new HashMap<>();
-                city.put("name", "Los Angeles");
-                city.put("state", "CA");
-                city.put("country", "USA");
-
-                System.out.println("Hello Login");
-                System.out.println(emailText);
-                System.out.println(passwordText);
-                mAuth.signInWithEmailAndPassword(emailText, passwordText)
-                        .addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
-                            @Override
-                            public void onComplete(@NonNull Task<AuthResult> task) {
-                                if (task.isSuccessful()) {
-                                    // Sign in success, update UI with the signed-in user's information
-                                    Log.d(TAG, "signInWithEmail:success");
-                                    FirebaseUser user = mAuth.getCurrentUser();
-                                    System.out.println("Login erfolgreich");https://youtube.com/watch?v=xonrW2smPyg&feature=shares&t=46
-                                    db.collection("routes").document(user.getUid()).set(city);
-                                } else {
-                                    Toast.makeText(LoginActivity.this, "Authentication failed.",
-                                            Toast.LENGTH_SHORT).show();
-                                    System.out.println("Login nicht erfolgreich");
-                                }
-                            }
-                        });
-            }
+            mAuth.signInWithEmailAndPassword(emailText, passwordText).addOnCompleteListener(LoginActivity.this, task -> {
+                if (task.isSuccessful()) {
+                    Toast.makeText(LoginActivity.this, "Login erfolgreich!", Toast.LENGTH_SHORT).show();
+                    startActivity(new Intent(this, MainActivity.class));
+                } else {
+                    Toast.makeText(LoginActivity.this, "Login fehlgeschlagen.", Toast.LENGTH_SHORT).show();
+                }
+            });
         });
     }
 
     @Override
     public boolean onSupportNavigateUp() {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_login);
-        return NavigationUI.navigateUp(navController, appBarConfiguration)
-                || super.onSupportNavigateUp();
+        return NavigationUI.navigateUp(navController, appBarConfiguration) || super.onSupportNavigateUp();
     }
 }
